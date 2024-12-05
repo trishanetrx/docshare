e2aadc6f5fff750a6a9e116f8870759d0d5c06e5
Trishane <136679393+trishanetrx@users.noreply.github.com>
2024-12-05 10:39:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://4.248.153.134/clipboard'; // Replace with your server's IP or domain
+const apiUrl = 'https://negombotech.com/clipboard'; // Replace with your server's IP or domain
 
 document.getElementById('saveButton').addEventListener('click', saveToClipboard);
 document.getElementById('loadButton').addEventListener('click', loadClipboard);
