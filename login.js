91ba0ba4fe26165a324b975e0f621f04b70f7c52
Trishane <136679393+trishanetrx@users.noreply.github.com>
2024-12-07 21:53:22 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,23 @@
 const apiUrl = 'https://negombotech.com/api'; // Define the API base URL
 
 // Add toggle password visibility functionality
-document.getElementById('togglePassword').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('togglePassword');
     const passwordInput = document.getElementById('password');
     const passwordIcon = document.getElementById('passwordIcon');
 
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        passwordIcon.classList.remove('fa-eye');
-        passwordIcon.classList.add('fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
-        passwordIcon.classList.remove('fa-eye-slash');
-        passwordIcon.classList.add('fa-eye');
+    if (toggleButton && passwordInput && passwordIcon) {
+        toggleButton.addEventListener('click', () => {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                passwordIcon.classList.remove('fa-eye');
+                passwordIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                passwordIcon.classList.remove('fa-eye-slash');
+                passwordIcon.classList.add('fa-eye');
+            }
+        });
     }
 });
 
